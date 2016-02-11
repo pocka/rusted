b9d7b59e37bc75c67e67e9851bbac81f619117f0
pocka <pocka.moe@gmail.com>
2016-02-11 20:28:50 +0900

    Add test for index.js

---
+++ test/index.js
@@ -0,0 +1,20 @@
+import {expect} from 'chai';
+
+import rusted from '../src/';
+import rusted_es5 from '../lib/';
+
+describe('index.js',()=>{
+	let apis=[
+		'Result','Ok','Err',
+		'Option','Some','None',
+		'Enum',
+		'match','impl',
+		'panic'
+	];
+	it('should importing and exporting properly',()=>{
+		expect(apis.some((api)=>!(api in rusted))).not.to.be.true;
+	});
+	it('should importing and exporting properly (compiled)',()=>{
+		expect(apis.some((api)=>!(api in rusted_es5))).not.to.be.true;
+	});
+});
