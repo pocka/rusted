99bad706c5bb0e3ea4456e09dc8ee2d0a594cc24
pocka <pocka.moe@gmail.com>
2016-02-12 20:23:43 +0900

    Simplify

--- src/trait.js
+++ src/trait.js
@@ -1,18 +1,14 @@
-import {is_empty_function} from './util';
-
 let factory=(block)=>{
-	let methods=Object.keys(block).map(key=>({key,value:block[key]}));
-
 	let trait={};
 
-	methods.forEach((method)=>{
-		Object.defineProperty(trait,method.key,{
-			value:method.value,
+	for(let name in block){
+		Object.defineProperty(trait,name,{
+			value:block[name],
 			enumerable:true,
 			writable:false,
 			configurable:false
 		});
-	});
+	}
 
 	return trait;
 };
