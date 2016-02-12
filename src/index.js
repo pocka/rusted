52f29c03c2b7798eb1445e12bf3afb435745aa58
pocka <pocka.moe@gmail.com>
2016-02-12 21:20:27 +0900

    Add exporting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Enum from './enum';
 import match from './match';
 import impl from './impl';
+import trait from './trait';
 import panic from './panic';
 import struct from './struct';
 import type from './type';
@@ -19,6 +20,7 @@ module.exports={
 	struct,
 	match,
 	impl,
+	trait,
 	panic,
 	type
 };
