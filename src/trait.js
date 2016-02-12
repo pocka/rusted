a89d8f18ff79435b91ea2bb40acf564d43eefad6
pocka <pocka.moe@gmail.com>
2016-02-12 20:20:56 +0900

    Change returned value
    Object contains attributes -> function body

--- src/trait.js
+++ src/trait.js
@@ -1,4 +1,4 @@
-import {is_static_method,is_empty_function} from './util';
+import {is_empty_function} from './util';
 
 let factory=(block)=>{
 	let methods=Object.keys(block).map(key=>({key,value:block[key]}));
@@ -7,11 +7,7 @@ let factory=(block)=>{
 
 	methods.forEach((method)=>{
 		Object.defineProperty(trait,method.key,{
-			value:{
-				is_static:is_static_method(method.value),
-				is_empty:is_empty_function(method.value),
-				body:method.value
-			},
+			value:method.value,
 			enumerable:true,
 			writable:false,
 			configurable:false
--- test/trait.js
+++ test/trait.js
@@ -16,37 +16,9 @@ describe('trait.js',()=>{
 			});
 
 			expect(Foo).to.have.property('foo')
-				.that.is.an('object');
+				.that.is.an('function');
 			expect(Foo).to.have.property('bar')
-				.that.is.an('object');
-		});
-		it('should returns object that have properties is_static,is_empty,body',()=>{
-			let empty_method=(self)=>{},
-				empty_static=()=>{},
-				normal_method=(self)=>{return true;},
-				static_method=()=>{return true;};
-			let Foo=trait({
-				empty_method,
-				empty_static,
-				normal_method,
-				static_method
-			});
-			expect(Foo.empty_method).to.deep.equal({
-				is_static:false,is_empty:true,
-				body:empty_method
-			});
-			expect(Foo.empty_static).to.deep.equal({
-				is_static:true,is_empty:true,
-				body:empty_static
-			});
-			expect(Foo.normal_method).to.deep.equal({
-				is_static:false,is_empty:false,
-				body:normal_method
-			});
-			expect(Foo.static_method).to.deep.equal({
-				is_static:true,is_empty:false,
-				body:static_method
-			});
+				.that.is.an('function');
 		});
 	});
 });
